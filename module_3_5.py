40a33212a3e8464ca7234a219fc79c683fc49da0
Сергей <98190914+serg-shteyn@users.noreply.github.com>
2024-10-01 12:17:39 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -5,9 +5,12 @@ def get_multiplied_digits (number):
 	first = int(str_number[0])
 	if len(str_number)>1:
 		return (first *    get_multiplied_digits(int(str_number[1:])))
-	else:
-		return first
+	else:	# удаление нуля, если он последний
+		if first != 0: 
+		    return first
+		else:
+			return True
 
 	
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(40030)
 print(result)
