c04562480d46401e9b54e313c84f422920d9643b
Sergey <shtem_x@mail.ru>
2024-10-09 00:14:29 +0300

    08.10

--- module_5_3.py
+++ module_5_3.py
@@ -4,45 +4,45 @@ class House:
 	def __init__(self,name,floor):
 		self.name=name
 		self.number_of_floors=floor
+
+	def other_check(self, other):
+		if isinstance(other, int):
+			return other
+		elif isinstance(other,House):
+			return other.number_of_floors
+
 		
 	def __len__(self):
 		return self.number_of_floors
 		
 	def __lt__(self,other):
-		return self.number_of_floors<other.number_of_floors
+		return self.number_of_floors<self.other_check(other)
 		
 	def __le__(self,other):
-		return self.number_of_floors<=other.number_of_floors
+		return self.number_of_floors<=self.other_check(other)
 		
 	def __gt__(self,other):
-		return self.number_of_floors>other.number_of_floors
+		return self.number_of_floors>self.other_check(other)
 		
 	def __ge__(self,other):
-		return self.number_of_floors>=other.number_of_floors
+		return self.number_of_floors>=self.other_check(other)
 		
 	def __eq__(self,other):
-		if isinstance(other,House):
-			return self.number_of_floors==other.number_of_floors
-		elif isinstance(other,int):
-			return self.number_of_floors==other
+		return self.number_of_floors==self.other_check(other)
+
 		
 	def __ne__(self,other):
-		return self.number_of_floors!=other.number_of_floors
+		return self.number_of_floors!=self.other_check(other)
 		
 	def __add__(self,value):
-		if isinstance(value,int):
-			self.number_of_floors=self.number_of_floors+value
-		elif isinstance(value,House):
-			self.number_of_floors=self.number_of_floors+value.number_of_floors
+		self.number_of_floors=self.number_of_floors+self.other_check(value)
 		return self
 		
 	def __radd__(self,value):
-		self.number_of_floors=self.number_of_floors+value
-		return self
+		return self.__add__(value)
 		
 	def __iadd__(self,value):
-		self.number_of_floors+=value
-		return self
+		return self.__add__(value)
 		
 	def __str__(self):
 		return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
@@ -62,7 +62,7 @@ print(h2)
 
 print(h1 == h2) # __eq__
 
-h1 = h1 + h2 # __add__
+h1 = h1 + 10 # __add__
 print(h1)
 print(h1 == h2)
 
