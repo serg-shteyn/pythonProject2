95be1627f3d62f748ef78419052f6f505a167860
Sergey <shtem_x@mail.ru>
2024-10-14 19:15:25 +0300

    08.10

--- module5hard.py
+++ module5hard.py
@@ -19,12 +19,17 @@ class Video:
 		self.duration=duration
 		self.time_now=time_now
 		self.adult_mode=adult_mode
+
+	def __contains__(self, title):
+		return title in self.title
+
 	
 class UrTube:
 	users=[]
 	videos=[]
 	current_user=None
-	
+
+
 	def register(self,nickname,password,age):
 		check=False
 		for i in self.users:
@@ -49,31 +54,39 @@ class UrTube:
 		print('До свидания!')
 		
 	def get_videos(self,search):
-		pass
+		result=[]
+		for i in self.videos:
+			if search.lower() in i.title.lower():
+				result.append(i.title)
+		return result
 		
 	def add(self, *args):
 		for i in args:
 			self.videos.append(i)
 		
-	def watch_video(self,title_film):
+	def watch_video(self,title_film,time_watch=20):
 		if self.current_user==None:
 			print('Войдите в аккаунт, чтобы смотреть видео')
 		else:
 			for j in self.videos:
-				if title_film in j.title :
+				if title_film in j :
 					if (j.adult_mode==True) and (self.current_user.age < 18):
 						print('Вам нет 18 лет, пожалуйста покиньте страницу')
 						break
 					else:
 						print(f"Идет просмотр фильма, {title_film}")
-						for i in range(10):
-							print(i)
+						stop_watch = j.time_now + time_watch
+						for i in range(j.time_now+1,j.duration+1):
 							time.sleep(1)
-						print("Конец видео")
-						break
-		
-	
-	
+							j.time_now=i
+							print(j.time_now)
+							if j.time_now==j.duration:
+								print('Конец видео')
+								j.time_now=0
+							elif j.time_now==stop_watch:
+								print(f'Просмотр остановлен на {j.time_now} секунде')
+								break
+
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
@@ -82,7 +95,8 @@ v2 = Video('Для чего девушкам парень программист
 ur.add(v1, v2)
 
 # Проверка поиска
-
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
 
 # Проверка на вход пользователя и возрастное ограничение
 ur.watch_video('Для чего девушкам парень программист?')
@@ -97,7 +111,6 @@ print(ur.current_user)
 
 # Попытка воспроизведения несуществующего видео
 ur.watch_video('Лучший язык программирования 2024 года!')
-
 
 
 
