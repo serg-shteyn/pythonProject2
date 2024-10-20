fa221870038bb8db1b5602c941dec9f03bbc769e
Сергей <98190914+serg-shteyn@users.noreply.github.com>
2024-10-20 21:59:41 +0300

    Add files via upload

--- module6hard.py
+++ module6hard.py
@@ -1,33 +1,77 @@
 # Дополнительное практическое задание по модулю: "Наследование классов."
+import math
 
 class Figure:
 	sides_count=0
+	filled = True
+	__color=(0,0,0)
+	__sides=0
 	
-	def __init__(self,sides,color,filled):
-		self.__sides=sides
-		self.__color=color
-		self.filled=filled
+	def __init__(self,color,*sides):
+		if self.__is_valid_color(color):
+			self.__color=color
 		
 	def get_color(self):
-		return self.color
+		return self.__color
 		
-	def is_valid_color(self,r,g,b):
-		if type(r)==int and type(g)==int and type(b)==int:
-			if r >=0 and r<=255 and g >=0 and g<=255 and b>=0 and b<=255:
-				print(True)
-				return True
+	def __is_valid_color(self,color):
+		for i in color:
+			if isinstance(i,int) and i>=0 and i<=255 :
+				pass	
 			else:
-				print(False,"диапазон 0-255")
 				return False
-		else:
-			print (False, "не целое число")
-			return False
+		if len(color)==3:
+		    return True
 			
-	def set_color(self,r,g,b):
-			if self.is_valid_color(r,g,b):
-				self.__color=(r,g,b)
+	def set_color(self,*color):
+		if self.__is_valid_color(color):
+			self.__color=color
 				
+#	def set_sides(self,*new_sides):
+#		if len(new_sides)==self.sides_count:
+#			self.__sides=new_sides
+			
+	def get_sides(self):
+		return self.__sides
+			
+				
+class Circle(Figure):	
+	sides_count=1
 	
+	def __init__(self,color,*sides):
+		super().__init__(color,sides)
+		self.set_sides(sides)
+		#self.__radius = self.get_radius(self.get_sides())
 		
-f=Figure((67,5,7),(3,54,89),True)
-f.is_valid_color(0,255,0)
+	def set_sides(self,new_sides):
+		print('₽',new_sides)
+		if len(new_sides)!=self.sides_count:
+			self.__sides=1
+		else:
+			self.__sides=new_sides
+		
+	def get_radius(self,side):
+		return side[0]/(2*math.pi)
+	
+	def get_square(self):
+		return math.pi*(self.get_radius(self.get_sides())**2)
+		
+		
+class Cube(Figure):
+	pass
+		
+circle1 = Circle((200, 200,200), 10,23) # (Цвет, стороны)
+print("f",circle1.get_sides())
+#cube1 = Cube((222, 35, 130), 6)
+print(circle1.get_color())
+# Проверка на изменение цветов:
+circle1.set_color(55,45, 77) # Изменится
+print(circle1.get_color())
+#cube1.set_color(300, 70, 15) # Не изменится
+#print(cube1.get_color())
+
+# Проверка на изменение сторон:
+#cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+#print(cube1.get_sides())
+circle1.set_sides(15) # Изменится
+print(circle1.get_sides())
