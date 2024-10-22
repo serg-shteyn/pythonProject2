5e28fe725d7cad79ac3912843831174679b15fa4
Sergey <shtem_x@mail.ru>
2024-10-22 23:46:03 +0300

    22.10 23:45 Режимы открытия файлов Задача: "Учет товаров"

--- module_7_1.py
+++ module_7_1.py
@@ -16,10 +16,7 @@ class Shop:
     __file_name = 'products.txt'
 
     def check_list(self,prod):
-        file = open(self.__file_name,'r')
-        str = file.read()
-        file.close()
-        if prod in str:
+        if prod in self.get_products():
             return False
         else:
             return True
@@ -27,11 +24,11 @@ class Shop:
     def add(self,*products):
         file = open(self.__file_name,'a')
         for i in products:
-            if self.check_list(i.__str__()):
-                file.write(f"{i.__str__()}\n")
-            else:
+            if i.__str__() in self.get_products():
                 print(f"Продукт {i} уже есть в магазине")
                 continue
+            else:
+                file.write(f"{i.__str__()}\n")
         file.close()
 
     def get_products(self):
