b969d092af7cd58789e1cdc2a37e3bd0866040b2
Sergey <shtem_x@mail.ru>
2025-01-03 23:58:45 +0300

    03.01.2025 23:58

--- module_11_1.py
+++ module_11_1.py
@@ -2,6 +2,8 @@ import requests
 import time
 from PIL import Image
 import os
+import pandas as pd
+import openpyxl
 
 
 #Работа с request
@@ -25,5 +27,14 @@ image=Image.open('img_2025.jpg')
 cropped=image.crop((0,0,800,200))
 #сохраняем обрезанное изображение
 cropped.save(os.path.join(os.getcwd(),'img_2025_small.jpg'))
+
+#pandas
+#Создание датафрейма вручную
+df=pd.DataFrame([[1,'a',3,4,5,6,7],[2,'b',13,14,15,16,17],[3,'c',18,19,20,21,22]],columns=['id','A','B','C','D','E','F'])
+#Сохраняем в файл csv
+df.to_csv('save_csv.csv',index=False)
+#Сохраняем в excel
+df.to_excel('save_exel.xlsx',index=False)
+
 
 
