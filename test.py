a1d0bfe2430caf68e71dc68fe228d352581bf0ee
Sergey <shtem_x@mail.ru>
2025-01-21 23:04:09 +0300

    07.01.2025 22:58

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
 import random
 
-secret = random.randint(1,101)
-my_i=0
-tr=0
-while secret!=my_i:
-    my_i=int(input('Введите число: '))
-    tr += 1
-    if secret < my_i:
-        print("меньше")
-    elif secret > my_i:
-        print("больше")
-    else:
-        print (f"Вы угадали! Секретное число {secret}, за {tr} попыток")
+
+print(pytz.all_timezones)
+tz_Vancouver = pytz.timezone('America/Vancouver')
+print(dt.now(tz_Vancouver))
+
