b32dba8681c32144175e0cdee4a9c87b7ad67968
Sergey <shtem_x@mail.ru>
2025-01-27 20:11:34 +0300

    27_01_2025

--- tests_12_4.py
+++ tests_12_4.py
@@ -1,57 +1,56 @@
 # Логирование
 import logging
 import unittest
-from tests_12_3 import RunnerTest
 
 class Runner:
-    def __init__(self, name, speed=5):
-        if isinstance(name, str):
-            self.name = name
-        else:
-            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
-        self.distance = 0
-        if speed > 0:
-            self.speed = speed
-        else:
-            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+	def __init__(self, name, speed=5):
+		if isinstance(name, str):
+			self.name = name
+		else:
+			raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+		self.distance = 0
+		if speed > 0:
+			self.speed = speed
+		else:
+			raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
 
-    def run(self):
-        self.distance += self.speed * 2
+	def run(self):
+		self.distance += self.speed * 2
 
-    def walk(self):
-        self.distance += self.speed
+	def walk(self):
+		self.distance += self.speed
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
-    def __repr__(self):
-        return self.name
+	def __repr__(self):
+		return self.name
 
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return self.name == other
-        elif isinstance(other, Runner):
-            return self.name == other.name
+	def __eq__(self, other):
+		if isinstance(other, str):
+			return self.name == other
+		elif isinstance(other, Runner):
+			return self.name == other.name
 
 
 class Tournament:
-    def __init__(self, distance, *participants):
-        self.full_distance = distance
-        self.participants = list(participants)
+	def __init__(self, distance, *participants):
+		self.full_distance = distance
+		self.participants = list(participants)
 
-    def start(self):
-        finishers = {}
-        place = 1
-        while self.participants:
-            for participant in self.participants:
-                participant.run()
-                if participant.distance >= self.full_distance:
-                    finishers[place] = participant
-                    place += 1
-                    self.participants.remove(participant)
+	def start(self):
+		finishers = {}
+		place = 1
+		while self.participants:
+			for participant in self.participants:
+				participant.run()
+				if participant.distance >= self.full_distance:
+					finishers[place] = participant
+					place += 1
+					self.participants.remove(participant)
 
-        return finishers
-        
+		return finishers
+
 class RunnerTest(unittest.TestCase):
 	is_frozen = False
 
@@ -64,10 +63,10 @@ class RunnerTest(unittest.TestCase):
 
 	@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
 	def test_run(self):
-	    tr=Runner('Name2')
-	    for i in range(10):
-	        tr.run()
-	    self.assertEqual(tr.distance,100)
+		tr=Runner('Name2')
+		for i in range(10):
+			tr.run()
+		self.assertEqual(tr.distance,100)
 
 	@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
 	def test_challenge(self):
@@ -86,6 +85,9 @@ first = Runner('Вося', 10)
 # print(t.start())
 
 if __name__ == '__main__':
-	logging.basicConfig(level='logging.INFO', filemode='w',filename='runner_tests.log',format='%(levelname)s ¦ %(message)s')
+	logging.basicConfig(level='logging.INFO', filemode='w',filename='runner_tests.log',
+                        format='%(levelname)s ¦ %(message)s',encoding='%(UTF-8)s')
+
+
 	
 	
