d81843ce17aed3abbf2bbc2303ce3b2414d014b4
Sergey <shtem_x@mail.ru>
2025-02-03 19:25:14 +0300

    03_02_2025

---
+++ main_1.py
@@ -0,0 +1,20 @@
+# 1st program
+
+print(9**0.5*5)
+
+# 2nd program
+
+print(9.9>9.8 and 1000 != 1000.1)
+
+# 3rd program
+
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+# 4th program
+
+a='123.456'
+a=float(a)*10
+a=str(int(a))
+print(a[-1])
